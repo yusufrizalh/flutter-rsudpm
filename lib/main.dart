500ce8e770900c7ca6087c070a4c3d6b9f8d7416
Yusuf Rizal <yusufrizalh@gmail.com>
2024-10-02 23:08:44 +0700

    change material color

--- lib/main.dart
+++ lib/main.dart
@@ -23,18 +23,18 @@ class MyApp extends StatefulWidget {
 }
 
 MaterialColor myCustomColor = MaterialColor(
-  const Color.fromRGBO(136, 111, 255, 1).value,
+  const Color.fromRGBO(10, 117, 173, 1).value,
   const <int, Color>{
-    50: Color.fromRGBO(136, 111, 255, 0.1),
-    100: Color.fromRGBO(136, 111, 255, 0.2),
-    200: Color.fromRGBO(136, 111, 255, 0.3),
-    300: Color.fromRGBO(136, 111, 255, 0.4),
-    400: Color.fromRGBO(136, 111, 255, 0.5),
-    500: Color.fromRGBO(136, 111, 255, 0.6),
-    600: Color.fromRGBO(136, 111, 255, 0.7),
-    700: Color.fromRGBO(136, 111, 255, 0.8),
-    800: Color.fromRGBO(136, 111, 255, 0.9),
-    900: Color.fromRGBO(136, 111, 255, 1),
+    50: Color.fromRGBO(10, 117, 173, 0.1),
+    100: Color.fromRGBO(10, 117, 173, 0.2),
+    200: Color.fromRGBO(10, 117, 173, 0.3),
+    300: Color.fromRGBO(10, 117, 173, 0.4),
+    400: Color.fromRGBO(10, 117, 173, 0.5),
+    500: Color.fromRGBO(10, 117, 173, 0.6),
+    600: Color.fromRGBO(10, 117, 173, 0.7),
+    700: Color.fromRGBO(10, 117, 173, 0.8),
+    800: Color.fromRGBO(10, 117, 173, 0.9),
+    900: Color.fromRGBO(10, 117, 173, 1),
   },
 );
 
